bae86dcc66363d6f6eddd9481cf6888b8fce1a52
Hristo <hrvasilev87@gmail.com>
2017-01-15 00:20:27 +0200

    initialize the project

---
+++ StartUp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartUp
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var a = "first commin to initialize the project";
+        }
+    }
+}
